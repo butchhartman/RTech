ccf3d911445bfe293f3af1504174450403827bff
butchhartman <rcamacho5436@gmail.com>
2025-09-27 10:51:04 -0700

    Updated error codes

--- src/rtEErrorCodes/rtEErrorCodes.h
+++ src/rtEErrorCodes/rtEErrorCodes.h
@@ -1,9 +1,15 @@
 #ifndef RTEERRORCODE_H_
 #define RTEERRORCODE_H_
 
+/* The error handling design philosphy:
+*       Distinct modules of code (rtEW, rtELog, etc) return error codes as a return value. From there, it is up to the user of that
+        module's functions to handle the error or pass it along themselves.
+*/
+
 enum rtEErrorCode {
         rtEErrorCode_SUCCESS,
         rtEErrorCode_MEMORY_ALLOC_FAILURE,
+        rtEErrorCode_PASSED_NULL_PTR,
 };
 
 #endif // RTEERRORCODE_H_
