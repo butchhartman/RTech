7d0fe77c514bfbc9192ab9f7714ac3816488fc5f
butchhartman <rcamacho5436@gmail.com>
2025-09-27 20:55:40 -0700

    Used my testing header to create tests for the windowing system. It's a good thing I did because I instantly found a bunch of problems :P

--- test/src/rtEW_test.c
+++ test/src/rtEW_test.c
@@ -1,11 +1,71 @@
+#include "rtEErrorCodes/rtEErrorCodes.h"
+#include "rtEW/rtenginewindow.h"
+#include <assert.h>
 #include <rTest/rTest.h>
-#include <stdio.h>
 
-STANDARD_TEST_CASE(Pooping) {
-        printf("Sample Test");
-}
+RTEST_SUITE_BEGIN("rtEW Tests") {
 
-int main() {
-        RUN_STANDARD_TEST_CASE(Pooping);
-        return 0;
+        RTEST_CASE("The module can start and cleanup without error") {
+                RTEST_ASSERT(rtEW_init() == rtEErrorCode_SUCCESS);
+
+                RTEST_ASSERT(rtEW_cleanup() == rtEErrorCode_SUCCESS);
+        }
+
+        RTEST_CASE("The module can start & cleanup repeatedly with no errors") {
+                RTEST_ASSERT(rtEW_init() == rtEErrorCode_SUCCESS);
+
+                RTEST_ASSERT(rtEW_cleanup() == rtEErrorCode_SUCCESS);
+
+                RTEST_ASSERT(rtEW_init() == rtEErrorCode_SUCCESS);
+
+                RTEST_ASSERT(rtEW_cleanup() == rtEErrorCode_SUCCESS);
+
+                RTEST_ASSERT(rtEW_init() == rtEErrorCode_SUCCESS);
+
+                RTEST_ASSERT(rtEW_cleanup() == rtEErrorCode_SUCCESS);
+
+                RTEST_ASSERT(rtEW_init() == rtEErrorCode_SUCCESS);
+
+                RTEST_ASSERT(rtEW_cleanup() == rtEErrorCode_SUCCESS);
+        }
+
+        rtEW_init(); 
+
+        RTEST_CASE("Windows can be created and cleaned up") {
+                struct rtEngineWindow* window;
+                RTEST_ASSERT(rtEW_createWindow(&window, "Test") == rtEErrorCode_SUCCESS);
+                RTEST_ASSERT(window != nullptr);
+
+                RTEST_ASSERT(rtEW_cleanupWindow(&window) == rtEErrorCode_SUCCESS);
+                RTEST_ASSERT(window == nullptr);
+        }
+
+        RTEST_CASE("Window can be shown and hidden to user") {
+                struct rtEngineWindow* window;
+                RTEST_ASSERT(rtEW_createWindow(&window, "Test") == rtEErrorCode_SUCCESS);
+
+                rtEW_showWindow(window);
+                rtEW_hideWindow(window);
+                rtEW_showWindow(window);
+                rtEW_hideWindow(window);
+                rtEW_showWindow(window);
+                rtEW_hideWindow(window);
+
+                RTEST_ASSERT(rtEW_cleanupWindow(&window) == rtEErrorCode_SUCCESS);
+                RTEST_ASSERT(window == nullptr);
+        }
+
+        RTEST_CASE("Window's should close state can be queried") {
+                // TODO : The should close state will NEVER be changed unless the message loop terminates 
+                // or the window is cleaned-up (in which case there is no longer a 'should close' variable to access. Do I need to test for this?
+                struct rtEngineWindow* window;
+                RTEST_ASSERT(rtEW_createWindow(&window, "Test") == rtEErrorCode_SUCCESS);
+
+                rtEW_showWindow(window);
+                RTEST_ASSERT(rtEW_windowShouldClose(window) == false);
+
+                RTEST_ASSERT(rtEW_cleanupWindow(&window) == rtEErrorCode_SUCCESS);
+                RTEST_ASSERT(window == nullptr);
+        }
+        rtEW_cleanup();
 }
