7830c8c0cecaa9907321ba86340e11511889e91f
butchhartman <rcamacho5436@gmail.com>
2025-10-04 09:09:33 -0700

    Modified the find block algorithm to use tail recursion because im awesome

--- src/rtEMemoryManager/procs/rtEMemoryManagerHelperProcs.c
+++ src/rtEMemoryManager/procs/rtEMemoryManagerHelperProcs.c
@@ -1,3 +1,4 @@
+#include "rtEErrorCodes/rtEErrorCodes.h"
 #include <rtEMemoryManager/procs/rtEMemoryManagerHelperProcs.h>
 #include <rtEMemoryManager/structs/rtEMemoryManagerStruct.h>
 #include <rtEMemoryManager/macros/rtEMMMacros.h>
@@ -6,51 +7,51 @@
 #include <stdlib.h>
 #include <string.h>
 
-enum rtEErrorCode rtEMM_findBlock(struct rtEMemoryManager* manager,  unsigned char** block, uint64_t blockSize) {
-        unsigned char* bufferPtr = manager->buff;
-        *block = nullptr;
-
-        while (*block == nullptr) {
-                printf("iter\n");
-                if (bufferPtr >= (manager->buff + manager->buffSize)) {
-                        printf("fail\n");
-                        return rtEErrorCode_MEMORY_ALLOC_FAILURE;
-                }
-
-                uint64_t headerBlockSize = GET_BUFFER_SIZE(bufferPtr);
-                uint64_t headerBlockOccupied = GET_BUFFER_IS_OCCUPIED(bufferPtr);
-                printf("found block size: %llu, occupied: %llu\n", headerBlockSize, headerBlockOccupied);
-
-                if (!headerBlockOccupied && headerBlockSize >= blockSize) {
-                        uint64_t deltaM = (bufferPtr - manager->buff);
-
-                        printf("delta M: %llu\n", deltaM);
-                        *block = bufferPtr;
-
-                        uint64_t header = blockSize | BUFFER_IS_OCCUPIED_BIT;
-
-                        memcpy(manager->buff + deltaM, &header, IN_BAND_HEADER_SIZE);
-                        //*(manager->buff + (deltaM)+ sizeof(uint32_t)) = 0xFF;
-
-                        uint64_t newSize = headerBlockSize - blockSize; 
-                        printf("ns: %llu\n", newSize);
-                        if (newSize != 0) {
-                                memcpy(manager->buff + deltaM + blockSize + IN_BAND_HEADER_SIZE, &newSize, IN_BAND_HEADER_SIZE);
-//                                *(manager->buff +deltaM + buffActualNeededSize + IN_BAND_HEADER_SIZE + sizeof(uint32_t)) = 0x00;
-                        } else {
-                                printf("No space for header, did not create\n");
-                        }
-
-                        *block += IN_BAND_HEADER_SIZE;
-
-                } else if (blockSize == 0) {
-                        return rtEErrorCode_MEMORY_ALLOC_FAILURE;
-                } else {
-                        bufferPtr += headerBlockSize + IN_BAND_HEADER_SIZE;
-                }
+static enum rtEErrorCode rtEMM_findBlockIter(struct rtEMemoryManager* manager, unsigned char** block, uint64_t blockSize, unsigned char* bufferPtr) {
+        printf("iter\n");
+        if (bufferPtr >= (manager->buff + manager->buffSize)) {
+                printf("fail\n");
+                return rtEErrorCode_MEMORY_ALLOC_FAILURE;
         }
 
-        return rtEErrorCode_SUCCESS;
+        uint64_t headerBlockSize = GET_BUFFER_SIZE(bufferPtr);
+        uint64_t headerBlockOccupied = GET_BUFFER_IS_OCCUPIED(bufferPtr);
+        printf("found block size: %llu, occupied: %llu\n", headerBlockSize, headerBlockOccupied);
+
+        if (!headerBlockOccupied && headerBlockSize >= blockSize) {
+                uint64_t deltaM = (bufferPtr - manager->buff);
+
+                printf("delta M: %llu\n", deltaM);
+                *block = bufferPtr;
+
+                uint64_t header = blockSize | BUFFER_IS_OCCUPIED_BIT;
+
+                memcpy(manager->buff + deltaM, &header, IN_BAND_HEADER_SIZE);
+
+                uint64_t newSize = headerBlockSize - blockSize; 
+                printf("ns: %llu\n", newSize);
+                if (newSize != 0) {
+                        memcpy(manager->buff + deltaM + blockSize + IN_BAND_HEADER_SIZE, &newSize, IN_BAND_HEADER_SIZE);
+//                                *(manager->buff +deltaM + buffActualNeededSize + IN_BAND_HEADER_SIZE + sizeof(uint32_t)) = 0x00;
+                } else {
+                        printf("No space for header, did not create\n");
+                }
+
+                *block += IN_BAND_HEADER_SIZE;
+                return rtEErrorCode_SUCCESS;
+
+        } else if (blockSize == 0) {
+                return rtEErrorCode_MEMORY_ALLOC_FAILURE;
+        }
+
+        bufferPtr += headerBlockSize + IN_BAND_HEADER_SIZE;
+        return rtEMM_findBlockIter(manager, block, blockSize, bufferPtr);
+}
+
+enum rtEErrorCode rtEMM_findBlock(struct rtEMemoryManager* manager,  unsigned char** block, uint64_t blockSize) {
+        *block = nullptr;
+
+        return rtEMM_findBlockIter(manager, block, blockSize, manager->buff);
 }
 
 enum rtEErrorCode rtEMM_dumpBlock(const char* outputDst, const unsigned char* block, size_t blockSize) {
